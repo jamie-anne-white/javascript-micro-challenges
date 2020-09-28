66fdfff1c6c30efed38898dccf6b9239777b86b2
Jamie Anne White <jamiewhite@Jamies-MBP.zyxel.com>
2020-09-28 16:43:24 -0500

    finished 130

--- files/client.js
+++ files/client.js
@@ -1,4 +1,5 @@
 console.log('test');
+array = [];
 
 // BASEMODE //
 
@@ -14,7 +15,7 @@ function addit(num1,num2) {
 console.log(addit(3,2)); // logs as 5
 console.log(addit(6,4)); // logs as 10
 
-/* 101 Conditionals
+/* 110 Conditionals
 Create a function that takes two numbers.
 
 If the first number is bigger than the second number, return `"The first number was bigger!"`.
@@ -37,3 +38,13 @@ function greaterThanConditional(num1,num2){
 console.log(greaterThanConditional(6,4)); // return "The first number was bigger!"
 console.log(greaterThanConditional(3,4)); // return "The second number was bigger!"
 console.log(greaterThanConditional(4,4)); // return "The numbers are the same!"
+
+/* 120 Arrays
+Create a function that takes in an array. Add up the first and last values in the array. */
+
+function firstAndLast(array) {
+    return array[0] + array[array.length - 1];
+  }
+  
+  console.log(firstAndLast([2,4,6,8,10])); // return 12
+  console.log(firstAndLast([3,6,9,12,15])); // return 18
