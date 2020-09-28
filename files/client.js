7243e747e4ec632bf81eb40905388f8023fc0bf8
Jamie Anne White <jamiewhite@Jamies-MBP.zyxel.com>
2020-09-28 13:37:33 -0500

    Completed 110 conditionals

--- files/client.js
+++ files/client.js
@@ -13,3 +13,27 @@ function addit(num1,num2) {
 
 console.log(addit(3,2)); // logs as 5
 console.log(addit(6,4)); // logs as 10
+
+/* 101 Conditionals
+Create a function that takes two numbers.
+
+If the first number is bigger than the second number, return `"The first number was bigger!"`.
+If the second number is bigger than the first number, return `"The second number was bigger!"`.
+If the numbers are the same, return `"The numbers are the same!"`.
+*/
+
+function greaterThanConditional(num1,num2){
+    if (num1 > num2){
+        return "The first number was bigger!"
+    }
+    else if (num1 < num2){
+        return "The second number was bigger!"
+    }
+    else {
+        return "The numbers are the same!"
+    }
+}
+
+console.log(greaterThanConditional(6,4)); // return "The first number was bigger!"
+console.log(greaterThanConditional(3,4)); // return "The second number was bigger!"
+console.log(greaterThanConditional(4,4)); // return "The numbers are the same!"
