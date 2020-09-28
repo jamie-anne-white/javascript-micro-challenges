35c94770590ed38244744d01f18d868338ba058d
Jamie Anne White <jamiewhite@Jamies-MBP.zyxel.com>
2020-09-28 13:28:57 -0500

    finished additions (100)

--- files/client.js
+++ files/client.js
@@ -1,1 +1,15 @@
 console.log('test');
+
+// BASEMODE //
+
+/* 100 - Additions 
+Create a function that takes two numbers.
+Your function should return the sum of the two numbers passed in.
+*/
+
+function addit(num1,num2) {
+    return Number(num1) + Number(num2);
+}
+
+console.log(addit(3,2)); // logs as 5
+console.log(addit(6,4)); // logs as 10
